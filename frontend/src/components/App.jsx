70f51b09d7b6de08fcf49973b57481bfb625c806
James Michel-Williams <jmichelwilliams@gmail.com>
2023-07-11 21:10:12 -0400

    added routes in App

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './Homepage';
+import Registration from './Registration';
+import Loginpage from './Loginpage';
+import Planner from './Planner';
 
-const App = () => <div>Garden Hero</div>;
+const App = () => {
+  console.log('app');
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/planner" element={<Planner />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
