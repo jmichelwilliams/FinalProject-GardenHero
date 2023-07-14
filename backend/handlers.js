f09575946c7bc21c4dbd7f0d3c7915010bbf4988
James Michel-Williams <jmichelwilliams@gmail.com>
2023-07-13 20:26:27 -0400

    Changed backend to server.js, created getCrops

---
+++ backend/handlers.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+
+require('dotenv').config({ path: '../.env' });
+
+const { MONGO_URI } = process.env;
+const dbName = process.env.DB_NAME;
+const collectionName = process.env.CROP_COLLECTION;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const getCrops = async (req, res) => {
+  const client = new MongoClient(MONGO_URI, options);
+  const db = client.db(dbName);
+
+  try {
+    await client.connect();
+    console.log('Connected!');
+
+    const collection = db.collection(collectionName);
+    const result = await collection.find().toArray();
+    console.log(result);
+    res.status(200).json({ status: 200, data: result });
+  } catch (error) {
+    res.status(500).json({ status: 500, error: 'Server Error' });
+  } finally {
+    client.close();
+    console.log('Disconnected');
+  }
+};
+
+module.exports = {
+  getCrops,
+};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const { getCrops } = require('./handlers');
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/crops', getCrops);
 
 // catch all endpoint
 app.get('*', (req, res) => {
