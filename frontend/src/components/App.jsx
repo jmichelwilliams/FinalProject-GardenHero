3eed1fb55961fd0ae77edfe2a6488f314ba58b8a
James Michel-Williams <jmichelwilliams@gmail.com>
2023-07-16 22:04:03 -0400

    removed wrapper

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import styled from 'styled-components';
 import Homepage from './Homepage';
 import Registration from './Registration';
 import Loginpage from './Loginpage';
@@ -22,27 +21,16 @@ const theme = createTheme({
 const App = () => (
   <Router>
     <GlobalStyle />
-    <Wrapper>
-      <ThemeProvider theme={theme}>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/login" element={<Loginpage />} />
-          <Route path="/planner" element={<Planner />} />
-        </Routes>
-      </ThemeProvider>
-    </Wrapper>
+
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/planner" element={<Planner />} />
+      </Routes>
+    </ThemeProvider>
   </Router>
 );
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: 0 auto;
-
-  @media (max-width: 768px) {
-    width: 85%;
-  }
-`;
 export default App;
