f26e1f4303cd6c26d3766c8668b1e9298eae8d7a
James Michel-Williams <jmichelwilliams@gmail.com>
2023-08-08 21:48:06 -0400

    Added useEffect to fetch weather

--- frontend/src/components/Planner.jsx
+++ frontend/src/components/Planner.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useAuth0 } from '@auth0/auth0-react';
 import StyledCalendar from './StyledCalendar';
@@ -9,12 +9,36 @@ import Wrapper from './Wrapper';
 const Planner = () => {
   const { user, isAuthenticated } = useAuth0();
   const [date, setDate] = useState(new Date());
-
+  const [weather, setWeather] = useState(null);
+  const apiKey = process.env.REACT_APP_API_KEY;
   const handleChange = (nextDate) => {
     setDate(nextDate);
   };
 
   const tileDisabled = ({ date: calendarDate }) => calendarDate < new Date();
+
+  useEffect(() => {
+    const getWeather = async () => {
+      const url =
+        'https://weatherapi-com.p.rapidapi.com/forecast.json?q=Montreal&days=3';
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': `${apiKey}`,
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setWeather(result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getWeather();
+  }, []);
 
   return (
     <Wrapper>
