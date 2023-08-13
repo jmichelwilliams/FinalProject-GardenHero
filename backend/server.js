f275cdd44278094736c4aae5ac466c9c640b844a
James Michel-Williams <jmichelwilliams@gmail.com>
2023-08-13 12:38:12 -0400

    created plantbox endpoints

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const { getAllCrops, getCrop } = require('./crop_handlers');
 const { logInUser } = require('./user_handlers');
 const { getWeather } = require('./weather_handlers');
+const { getUserPlantbox, addToGarden } = require('./plantbox_handlers');
 const app = express();
 
 app.use(morgan('tiny'));
@@ -14,6 +15,8 @@ app.get('/crops', getAllCrops);
 app.get('/crop/:cropname', getCrop);
 app.post('/login', logInUser);
 app.get('/weather', getWeather);
+app.get('/plantbox/:sub', getUserPlantbox);
+app.patch('/plantbox/:sub', addToGarden);
 // catch all endpoint
 app.get('*', (req, res) => {
   res.status(404).json({
