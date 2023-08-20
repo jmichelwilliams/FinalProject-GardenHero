b2e009f63c41c4ee011c2f4df50f88df2a2a3359
James Michel-Williams <jmichelwilliams@gmail.com>
2023-08-20 12:41:43 -0400

    Created DialogWindow, SmallLoginButton

---
+++ frontend/src/components/DialogWindow.jsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import SmallLoginButton from './SmallLoginButton';
+
+const DialogWindow = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        onClick={handleClickOpen}
+        variant="contained"
+        color="primary"
+        sx={{
+          height: '60px',
+          width: '150px',
+          marginRight: '8px',
+        }}
+      >
+        Go to Planner
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Login Required</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Please login to get access to the Planner.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <SmallLoginButton buttonText="Log in" />
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default DialogWindow;
---
+++ frontend/src/components/SmallLoginButton.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import Button from '@mui/material/Button';
+import PropTypes from 'prop-types';
+
+const SmallLoginButton = ({ buttonText }) => {
+  const { loginWithRedirect, isLoading } = useAuth0();
+  const handleLogin = async () => {
+    if (isLoading) {
+      return;
+    }
+    try {
+      await loginWithRedirect();
+    } catch (error) {
+      console.error('Error with login:', error);
+    }
+  };
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      onClick={handleLogin}
+      sx={{
+        height: '36px',
+        width: '80px',
+        marginRight: '8px',
+      }}
+    >
+      {buttonText}
+    </Button>
+  );
+};
+
+SmallLoginButton.propTypes = {
+  buttonText: PropTypes.string,
+};
+
+SmallLoginButton.defaultProps = {
+  buttonText: 'Login / Register',
+};
+
+export default SmallLoginButton;
