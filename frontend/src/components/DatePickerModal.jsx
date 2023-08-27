168d6358d12141a3b510ab1bb9d505fb07690590
James Michel-Williams <jmichelwilliams@gmail.com>
2023-08-27 11:54:07 -0400

    Refactored the quickadd in the datepicker

--- frontend/src/components/DatePickerModal.jsx
+++ frontend/src/components/DatePickerModal.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs from 'dayjs';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import Button from '@mui/material/Button';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { CircularProgress } from '@mui/material';
 import PropTypes from 'prop-types';
 
 // Component that renders a modal to allow user to pick a date
@@ -23,27 +25,39 @@ const DatePickerModal = ({
     onClose();
   };
 
+  // Get today's date
+  const today = dayjs();
+
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Pick a Date</DialogTitle>
-      <DialogContent>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker onChange={(date) => setSelectedDate(date)} />
-        </LocalizationProvider>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Cancel
-        </Button>
-        <Button
-          onClick={() => handleAddToGardenAndClose()}
-          variant="contained"
-          size="small"
-          color="primary"
-        >
-          Add to garden
-        </Button>
-      </DialogActions>
+      {selectedCrop ? (
+        <>
+          <DialogTitle>Plant {selectedCrop.name} on:</DialogTitle>
+          <DialogContent>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                defaultValue={today}
+                onChange={(date) => setSelectedDate(date)}
+              />
+            </LocalizationProvider>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onClose} color="primary">
+              Cancel
+            </Button>
+            <Button
+              onClick={handleAddToGardenAndClose}
+              variant="contained"
+              size="small"
+              color="primary"
+            >
+              Add to garden
+            </Button>
+          </DialogActions>
+        </>
+      ) : (
+        <CircularProgress size={20} />
+      )}
     </Dialog>
   );
 };
