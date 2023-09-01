e2a2e20bb909474cdcb0ed9c01d5073c0d10dab1
James Michel-Williams <jmichelwilliams@gmail.com>
2023-09-01 17:38:30 -0400

    fixing heroku bug

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { getAllCrops, getCrop } = require('./crop_handlers');
 const { logInUser } = require('./user_handlers');
@@ -17,7 +17,7 @@ const app = express();
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(cors());
+// app.use(cors());
 // crops
 app.get('/crops', getAllCrops);
 app.get('/crop/:cropname', getCrop);
